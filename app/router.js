4a5a680fc684cfad210baaae3bc10ed2252eea1d
Oswaldo Acauan <oswaldoacauan@gmail.com>
2015-07-02 15:36:29 -0300

    2nd try

--- app/router.js
+++ app/router.js
@@ -1,11 +1,15 @@
 import Ember from 'ember';
 import config from './config/environment';
 
+import RouteMapper from 'core/utils/route-mapper';
+
+
 var Router = Ember.Router.extend({
   location: config.locationType
 });
 
 Router.map(function() {
+  RouteMapper(this, config.modulePrefix);
 });
 
 export default Router;
---
+++ tests/unit/utils/route-mapper-test.js
@@ -0,0 +1,10 @@
+import routeMapper from '../../../utils/route-mapper';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | route mapper');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = routeMapper();
+  assert.ok(result);
+});
